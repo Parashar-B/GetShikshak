053d47f216c99c31a53f6f4ab616aca496453b51
Rachna <rachnaag1999@gmail.com>
2023-03-27 18:56:48 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+import NavComponent from "./Components/NavComponent/NavComponent"
+function App() {
+
+  return (
+    <div className="App">
+     <NavComponent/>
+    </div>
+  )
+}
+
+export default App
